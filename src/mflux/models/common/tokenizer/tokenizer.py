400fb7182cf22cb06f85af7a466f6acd51914d47
kmtbang <zhongtian@gmail.com>
2026-03-10 22:48:08 +0800

    bug fix tokenizer

--- src/mflux/models/common/tokenizer/tokenizer.py
+++ src/mflux/models/common/tokenizer/tokenizer.py
@@ -118,7 +118,8 @@ class LanguageTokenizer(BaseTokenizer):
             return_overflowing_tokens=False,
             return_tensors="np",
         )
-        print(f"input len:{len(tokens["input_ids"])}")
+        p_cnt = len(tokens["input_ids"])
+        print(f"input len:{p_cnt}")
         input_ids = mx.array(tokens["input_ids"])
         if self.return_attention_mask:
             attention_mask = mx.array(tokens["attention_mask"])
